46f248d34d54b9b1ae1f1f5259e8bde530089a0f
Jason Godesky <jason.godesky@heysparkbox.com>
2023-02-06 11:14:38 -0500

    feat: create: Create element with classes

--- src/create.spec.ts
+++ src/create.spec.ts
@@ -1,9 +1,17 @@
 import { expect } from 'chai'
+import hasClass from './has-class'
 import create from './create'
 
 describe('create', () => {
   it('creates an element', () => {
     const el = create('p')
     expect(el.tagName).to.equal('P')
+  })
+
+  it('creates an element with the classes given', () => {
+    const classes = ['a', 'b', 'c']
+    const el = create('p', classes.slice(0, 2))
+    const checks = classes.map(className => hasClass(el, className))
+    expect(JSON.stringify(checks)).to.equal('[true,true,false]')
   })
 })
--- src/create.ts
+++ src/create.ts
@@ -1,3 +1,5 @@
-export default function create (tag: string): HTMLElement {
-  return document.createElement(tag)
+export default function create (tag: string, classes?: string[]): HTMLElement {
+  const el = document.createElement(tag)
+  if (classes !== undefined) el.classList.add(...classes)
+  return el
 }
