59d9fb80197466a71cfdea8152039cdec7458fed
Jason Godesky <jason.godesky@heysparkbox.com>
2023-02-06 11:46:06 -0500

    fix: Fix linting error

--- src/has-class.ts
+++ src/has-class.ts
@@ -1,4 +1,4 @@
-export default function hasClass(el: HTMLElement, ...classes: string[]): boolean {
+export default function hasClass (el: HTMLElement, ...classes: string[]): boolean {
   for (const className of classes) {
     if (el.classList.contains(className)) return true
   }
