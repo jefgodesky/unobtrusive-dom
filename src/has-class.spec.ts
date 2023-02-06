84bd4c85a6f6d3065c93415a2c3b61b69a96feea
Jason Godesky <jason.godesky@heysparkbox.com>
2023-02-06 11:06:31 -0500

    feat: Add new method: hasClass
    Tests if an element has any of the classes given.

---
+++ src/has-class.spec.ts
@@ -0,0 +1,22 @@
+import { expect } from 'chai'
+import hasClass from './has-class'
+
+describe('hasClass', () => {
+  it('returns true if the element has the class', () => {
+    const el = document.createElement('p')
+    el.classList.add('test')
+    expect(hasClass(el, 'test')).to.equal(true)
+  })
+
+  it('returns false if the element does not have the class', () => {
+    const el = document.createElement('p')
+    el.classList.add('test')
+    expect(hasClass(el, 'nope')).to.equal(false)
+  })
+
+  it('checks any number of classes', () => {
+    const el = document.createElement('p')
+    el.classList.add('test')
+    expect(hasClass(el, 'nope', 'nada', 'zilch', 'test', 'maybe')).to.equal(true)
+  })
+})
---
+++ src/has-class.ts
@@ -0,0 +1,6 @@
+export default function hasClass(el: HTMLElement, ...classes: string[]): boolean {
+  for (const className of classes) {
+    if (el.classList.contains(className)) return true
+  }
+  return false
+}
