7aa34931774da0ee7116ffe9d21db65c84f95939
Jason Godesky <jason.godesky@heysparkbox.com>
2023-02-13 10:30:58 -0500

    chore: Configure rollup

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import pkg from './package.json' assert { type: 'json' }
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+      name: pkg.name
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    commonjs(),
+    typescript({ tsconfig: './tsconfig.json' })
+  ]
+}
