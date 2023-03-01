d4f7a1ff6867c3d71e35ab57f5b29e664ca6a4d7
Jason Godesky <jefgodesky@gmail.com>
2023-03-01 13:28:21 -0500

    Add declaration file

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+import { CreateOptions } from './src/create'
+
+export as namespace unobtrusiveDom
+
+export function create(options: CreateOptions): HTMLElement
+export function addClass (el: HTMLElement, ...classes: string[]): void
+export function hasClass (el: HTMLElement, ...classes: string[]): boolean
+export function removeClass (el: HTMLElement, ...classes: string[]): void
+export function toggleClass (el: HTMLElement, ...classes: string[]): void
+export { CreateOptions }
